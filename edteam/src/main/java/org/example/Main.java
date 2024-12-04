48de36bcc83f9bad2e4bc0cc0092c6c5de1b5e03
borispacex <borisvargaspaucara@gmail.com>
2024-12-04 06:29:06 -0400

    idiomas

--- edteam/src/main/java/org/example/Main.java
+++ edteam/src/main/java/org/example/Main.java
@@ -1,30 +1,18 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Optional;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Main {
     public static void main(String[] args) {
 
-        Optional album = getAlbum("Random Access Memories");
-        if(album.isPresent()){
-            System.out.println("Se encontró el album");
-        }else{
-            System.out.println("No se encontró :(");
-        }
+        String pais = "ES";
+        String idioma = "es";
+        Locale localidad = new Locale(idioma,pais);
+        ResourceBundle mensajes = ResourceBundle.getBundle("lang",localidad);
+        System.out.println(mensajes.getString("edteam.lunch"));
+        System.out.println(mensajes.getString("edteam.kamekameha"));
 
-    }
-
-    private static Optional<Album> getAlbum(String nombre) {
-        HashMap lista = new HashMap<>();
-        lista.put("Thriller",new Album("Thriller","Michael Jackson","Epic Records"));
-        lista.put("Pasadena",new Album("Pasadena","Guns N Roses","Epic Records"));
-        lista.put("Meteora",new Album("Meteora","Linkin park","Epic Records"));
-        Optional album = Optional.empty();
-        if(lista.containsKey(nombre)){
-            album = Optional.of(lista.get(nombre));
-        }
-        return album;
     }
 
 }
