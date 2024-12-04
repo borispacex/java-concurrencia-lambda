2ebecbb11bf5942d6601f66677deb57474b742b8
borispacex <borisvargaspaucara@gmail.com>
2024-12-04 06:40:59 -0400

    lambdas

--- edteam/src/main/java/org/example/Main.java
+++ edteam/src/main/java/org/example/Main.java
@@ -1,30 +1,24 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        Optional album = getAlbum("Random Access Memories");
-        if(album.isPresent()){
-            System.out.println("Se encontró el album");
-        }else{
-            System.out.println("No se encontró :(");
-        }
+        List<User> lista = new ArrayList<>();
+        lista.add(new User("Lucas","López","joselopez@correo.com"));
+        lista.add(new User("Pedro","Sánchez","pedrosanchez@correo.com"));
+        lista.add(new User("Juan","Vázquez","juanvazquez@correo.com"));
+        lista.sort((User userA,User userB) -> userA.getEmail().compareTo(userB.getEmail()));
+        lista.forEach(user -> {
+            if(user.getNombre() == "Lucas"){
+                System.out.println("Hola "+user.getNombre());
+            }else{
+                System.out.println(user.getNombre());
+            }
+        });
 
-    }
-
-    private static Optional<Album> getAlbum(String nombre) {
-        HashMap lista = new HashMap<>();
-        lista.put("Thriller",new Album("Thriller","Michael Jackson","Epic Records"));
-        lista.put("Pasadena",new Album("Pasadena","Guns N Roses","Epic Records"));
-        lista.put("Meteora",new Album("Meteora","Linkin park","Epic Records"));
-        Optional album = Optional.empty();
-        if(lista.containsKey(nombre)){
-            album = Optional.of(lista.get(nombre));
-        }
-        return album;
     }
 
 }
